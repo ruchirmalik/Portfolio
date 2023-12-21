4baddd8306a75b545cb7426413ad14f27a185209
ruchirmalik <ruchir28@students.cs.ubc.ca>
2023-12-20 18:27:37 -0800

    fourth commit - enabled typewriter effect on Intro

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import './intro.css';
 import { Link } from 'react-scroll';
-import hireMeImg from '../../Assets/hireMe.png'
+import hireMeImg from '../../Assets/hireMe.png';
+import Typewriter from 'typewriter-effect';
+
+//<Typewriter options={{strings: ['Front-End Developer', 'Full-Stack Software Engineer'], autoStart: true, loop: true, }}/>
+{/* <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString('Hello World!')
+      .callFunction(() => {
+        console.log('String typed out!');
+      })
+      .pauseFor(2500)
+      .deleteChars(0)
+      .callFunction(() => {
+        console.log('All strings were deleted');
+      })
+      .start();
+  }}
+/> */}
+
 
 const Intro = () => {
   return (
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello,</span>
-        <span className="introText"> I'm <span className="introName">Ruchir </span> <br/> a new grad Software Engineer. </span>
+        <span className="introText"> I'm <span className="introName">Ruchir </span> 
+          <Typewriter onInit={(typewriter) => {
+              typewriter.typeString('a new grad Front-End Developer')
+              .callFunction(() => {
+              console.log('String typed out!');
+              })
+              .pauseFor(1000)
+              .deleteChars(19)
+              .changeDeleteSpeed(10)
+              typewriter.typeString('Full-Stack Software Engineer.')
+              .callFunction(() => {
+              console.log('String typed out!');
+              })
+              .callFunction(() => {
+              console.log('All strings were deleted');
+              })
+              .start();
+            }} /> 
+        </span>
         <p className="introPara"> I turn coffee into code and ideas into innovation. </p>
         <Link><button className="btn"><img src={hireMeImg} alt="" className="btnImg"/>Hire Me</button></Link>
       </div>
