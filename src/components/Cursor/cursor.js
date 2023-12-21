69ed8205eb6df15249e10ebbf7a6f9b7e6be79e2
ruchirmalik <ruchir28@students.cs.ubc.ca>
2023-12-21 05:45:13 -0800

    Fixed minor bug in Component Cursor

--- src/components/Cursor/cursor.js
+++ src/components/Cursor/cursor.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import useCustomCursor from '../../hooks/useCustomCursor';
+import { motion } from 'framer-motion';
 import './cursor.css';
 
 
 const CustomCursorComponent = () => {
   const cursorPosition = useCustomCursor();
 
+
   return (
     <div
-      style={{
+        style={{
         position: 'fixed',
         top: 0,
         left: 0,
-        width: '20px',
-        height: '20px',
+        width: '32px',
+        height: '32px',
         borderRadius: '50%',
         background: 'red',
-        transform: `translate(${cursorPosition.x}px, ${cursorPosition.y}px)`,
-      }}
-    />
+        transform: `translate(${cursorPosition.x - 16}px, ${cursorPosition.y - 16}px)`
+        
+      }}>
+       
+    </div>
   );
 };
 
