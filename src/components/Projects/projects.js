e36aca010d235c182564ab3c3e6f86d8c1d98bba
ruchirmalik <ruchir28@students.cs.ubc.ca>
2024-01-04 21:50:17 -0800

    NavBar, ContactPage completed and fixed -- responsiveness left

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -12,7 +12,7 @@ const Projects = () => {
           <div className="individualProject">
             <img src="" alt="" className="projectImg" />
             <div className="projectInfo">
-              <h1 className="projectName">Zenith Blogs: A Full-Stack Software Engineering Project </h1>
+              <h1 className="projectName">Zenith Blogs: A PWC-sponsored Software Engineering Project </h1>
               <p className="projectDesc">A PWC-sponsored project, Zenith Blogs is a comprehensive full-stack web blog application that represents collaborative efforts of a talented team of nine students. <br /> <br />
                 •	Built and containerized in Docker;supports user authentication, posting, deleting, liking and commenting on blogs; enabled pagination for enhanced blog navigation. <br />
                 •	Leveraged Java & Spring Boot for backend architecture, React.js for frontend development; employed Redux for state management. <br />
@@ -63,7 +63,24 @@ const Projects = () => {
             <img src="" alt="" className="projectImg" />
             <div className="projectInfo">
               <h2 className="projectName">Fun-Town Sports Center </h2>
-              <p className="projectDesc">More info here</p>
+              <p className="projectDesc">•	Formulated an E/R diagram for a sports centre & translated it into a relational schema to establish a robust database foundation.<br />
+                •	Recognized functional dependencies, applying the BCNF normalization technique to minimize data redundancy and optimize database efficiency.<br />
+                •	Orchestrated the development of a responsive website using JS, PHP, HTML, and CSS, coupled with MySQL for seamless data retrieval.<br />
+                •	Implemented an appointment management system, empowering coaches to create and delete appointments, enabling members to view, book, and cancel appointments with various coaches, and granting administrative oversight for comprehensive operations control<br />
+              </p>
+
+              <div className="tags">
+
+
+                <p className="tag">TypeScript</p>
+                <p className="tag">PHP</p>
+                <p className="tag">HTML</p>
+                <p className="tag">CSS</p>
+                <p className="tag">Data Normalization</p>
+                <p className="tag">E/R Diagram</p>
+                <p className="tag">Relational Schema</p>
+
+              </div>
             </div>
           </div>
 
