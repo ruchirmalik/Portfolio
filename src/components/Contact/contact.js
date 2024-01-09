ac663e2c53da3f3b6d324f35b1a9b38143096792
ruchirmalik <ruchir28@students.cs.ubc.ca>
2024-01-09 03:24:45 -0800

    Deleted custom form validation, fixed html form validation

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './contact.css';
 import { Link } from 'react-scroll';
 import emailjs from '@emailjs/browser';
@@ -8,16 +8,61 @@ import github from '../../Assets/GitHub.png';
 import linkedin from '../../Assets/linkedin.png';
 
 const Contact = () => {
-    const form = useRef();
+    const form = useRef()
+    const [sent, setSent] = useState(false)
+    // const [email, setEmail] = useState("")
+    // const [name, setName] = useState("")
+    // const [message, setMessage] = useState("")
+    // const [subject, setSubject] = useState("")
+    // const [emailError, setEmailError] = useState("")
+    // const [nameError, setNameError] = useState("")
+    // const [subjectError, setSubjectError] = useState("")
+    // const [messageError, setMessageError] = useState("")
+    const [emailSendError, setEmailSendError] = useState("")
+
+    const isValidEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
 
     const sendEmail = (e) => {
         e.preventDefault();
+        // setNameError("")
+        // setEmailError("")
+        // setMessageError("")
+        // setSubjectError("")
+
+        // if (!name) {
+        //     setNameError("Please enter a valid name")
+        //     return;
+        // }
+
+        // if (!email || !isValidEmail(email)) {
+        //     setEmailError("Please enter a valid email")
+        //     return;
+        // }
+
+
+        // if (!subject) {
+        //     setSubjectError("Please enter a valid subject")
+        //     return;
+        // }
+
+        // if (!message) {
+        //     setMessageError("Please enter a valid message")
+        //     return;
+        // }
 
         emailjs.sendForm('service_cp2kptb', 'template_3yqdatf', form.current, '2fRjLe2mdyV0pxCrR')
             .then((result) => {
                 console.log(result.text);
+                setSent(true);
             }, (error) => {
                 console.log(error.text);
+                setEmailSendError(`Couldn't send your email due to some error! Try again later :( ${error.text}`)
             });
     };
 
@@ -34,17 +79,24 @@ const Contact = () => {
                         <a href="https://github.com/ruchirmalik" target="_blank"> <img src={github} alt="Github_Logo" className="linkImg" /> </a>
 
                     </div>
-
                     <div className="rightHalf">
-                        <div className="contactMeDesc">I'd love to hear from you! Please fill out the form below and I'll get back to you as soon as possible.  </div> <br />
-                        <form action="" className="contactForm" ref={form} onSubmit={sendEmail}>
-                            <input type="text" className="name" placeholder='Your Name' name='your_name' />
-                            <input type="email" className="email" placeholder='Your Email' name='your_email' />
-                            <input type="text" className="subject" placeholder='Subject' name='your_subject' />
-                            <textarea className='msg' name="message" rows="5" placeholder='Your Message' ></textarea>
-                            <button className="submitBtn" type='submit' value='Send'>Submit</button>
-                        </form>
-
+                        {sent ? <h2>Thanks for reaching out! I'll get back to you soon</h2> :
+                            emailSendError ? <h2>{emailSendError}</h2> :
+                                <>
+                                    <div className="contactMeDesc">I'd love to hear from you! Please fill out the form below and I'll get back to you as soon as possible.  </div> <br />
+                                    <form action="" ref={form} onSubmit={sendEmail}>
+                                        <input type="text" className="name" placeholder='Your Name' name='your_name' required />
+                                        {/* {nameError && <p>{nameError}</p>} */}
+                                        <input type="email" className="email" placeholder='Your Email' name='your_email' required />
+                                        {/* {emailError && <p>{emailError}</p>} */}
+                                        <input type="text" className="subject" placeholder='Subject' name='your_subject' required />
+                                        {/* {subjectError && <p>{subjectError}</p>} */}
+                                        <textarea className='msg' name="message" rows="5" placeholder='Your Message' required ></textarea>
+                                        {/* {messageError && <p>{messageError}</p>} */}
+                                        <button className="submitBtn" type="submit">Submit</button>
+                                    </form>
+                                </>
+                        }
                     </div>
 
                 </div>
